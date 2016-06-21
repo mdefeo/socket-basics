634bda563eb7485268b88f2dd458542183b0aad8
Marcello <marcellodefeo@gmail.com>
2016-06-21 16:53:14 -0400

    Styling

--- public/js/app.js
+++ public/js/app.js
@@ -2,23 +2,30 @@ var socket 		=	io(),
 	$form		= 	$('#message-form'),
 	$message 	=	$('input[name="message"]'),
 	$response	=	$('#response'),
-	$msg  		=	'';
+	msg 		=	'',
+	scrolled 	= 	false;
 
 socket.on('connect', function() {
 	console.log('Connected to socket.io server.');
 });
 
 socket.on('message', function(message) {
-	console.log('New message:');
-	$response.append('<p>' + message.text + '</p>');
+		console.log('New message:');
+		$response.append('<p><span class="icon"></span>' + message.text + '<br><i>' + message.time + '</i></p>');
 });
 
 $(function() {
 	$form.on('submit', function(e) {
 		e.preventDefault();
-		socket.emit('message', {
-			text: $message.val()
-		});
-		$message.val('');
-	});
+		msg 	=	$message.val();
+		if(msg.length > 0) {
+			socket.emit('message', {
+				text: msg
+			});
+			$message.val('');
+			var res = document.getElementById("response");
+			res.scrollTop = res.scrollHeight;
+		}
+	});        
 });
+
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var PORT 		=	process.env.PORT || 3000,
 	express 	=	require('express'),
 	app 		=	express(),
 	http 		=	require('http').Server(app),
-	io	 		=	require('socket.io')(http);
+	io	 		=	require('socket.io')(http),
+	moment		=	require('moment'),
+	curTime;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,6 +14,7 @@ io.on('connection', function(socket) {
 		console.log('Message received: ' + message.text);
 		//sends to everyone but sender
 		//socket.broadcast.emit('message', message);
+		message.time =	moment().local().format('h:mm:ss a');  
 		io.emit('message',message);
 	});
 
