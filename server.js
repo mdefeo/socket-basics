7c5b528bfdf0128501a3bef898795bf5d407598e
Marcello <marcellodefeo@gmail.com>
2016-06-21 13:29:22 -0400

    Initial commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var PORT 		=	process.env.PORT || 3000,
 
 app.use(express.static(__dirname + '/public'));
 
+io.on('connection', function() {
+	console.log('User connected via socket.io.');
+});
+
 
 http.listen(PORT, function() {
 	console.log('Server started.');
