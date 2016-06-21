1eb69facf6d17abd7f258d4e39f26dad35e10c6f
Marcello <marcellodefeo@gmail.com>
2016-06-21 13:40:14 -0400

    IO

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# socket-basics
+# Socket Basics
+
+Creating a chat app using Socket.io
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var PORT 		=	process.env.PORT || 3000,
 	express 	=	require('express'),
 	app 		=	express(),
-	http 		=	require('http').Server(app);
+	http 		=	require('http').Server(app),
+	io	 		=	require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function() {
-	console.log('User connected via socket.io.');
+	console.log('User connected via');
 });
 
 
