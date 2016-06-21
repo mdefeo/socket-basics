88388b321bf1c5a5964142f43961c61ffb254fc1
Marcello <marcellodefeo@gmail.com>
2016-06-21 14:44:59 -0400

    Front end form

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,19 @@ socket.on('message', function(message) {
 	console.log('New message:');
 	console.log(message.text);
 });
+
+$(function() {
+	var $form		= 	$('#message-form'),
+		$message 	=	$('input[name="message"]'),
+		$response	=	$('#response'),
+		$msg  		=	'';
+	$form.on('submit', function(e) {
+		e.preventDefault();
+		$msg 		=	$message.val() + "<br>";
+		//$response.append($msg);
+		socket.emit('message', {
+			text: $msg
+		});
+		$message.val('');
+	});
+});
