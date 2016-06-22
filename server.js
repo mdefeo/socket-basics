c772b594261fb6b9cc7dfa2b082a8dd9e928024d
Marcello <marcellodefeo@gmail.com>
2016-06-22 10:09:26 -0400

    Styling

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var PORT 		=	process.env.PORT || 3000,
 	app 		=	express(),
 	http 		=	require('http').Server(app),
 	io	 		=	require('socket.io')(http),
+	sanitize 	= 	require('sanitize-html'),
 	moment		=	require('moment'),
 	curTime;
 
@@ -11,10 +12,11 @@ app.use(express.static(__dirname + '/public'));
 io.on('connection', function(socket) {
 
 	socket.on('message', function(message) {
+		message.text 	=	sanitize(message.text);
 		console.log('Message received: ' + message.text);
 		//sends to everyone but sender
 		//socket.broadcast.emit('message', message);
-		message.time =	moment().local().format('h:mm:ss a');  
+		message.time 	=	moment().local().format('h:mm:ss a');  
 		io.emit('message',message);
 	});
 
